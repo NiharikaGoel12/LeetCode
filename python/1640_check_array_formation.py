4d832495fb24a4a13b8f035dcafd3d592203ba56
Niharika Goel <niharika.coer@gmail.com>
2021-03-21 12:16:04 +0530

    1640 Check array formation through concatention-IMP

--- python/1640_check_array_formation.py
+++ python/1640_check_array_formation.py
@@ -5,17 +5,26 @@ class Solution(object):
         :type pieces: List[List[int]]
         :rtype: bool
         """
-        counter =0
-        for i in arr:
-            for j in pieces:
-                if i in j:
-                    counter +=1
 
-        if counter == len(arr):
-            return True
-        else:
-            return False
+        pieces_dict= {}
+        index =0
 
+        for i in pieces:
+            if i[0] not in pieces_dict:
+                pieces_dict[i[0]]=i
+
+        while index < len(arr):
+            if arr[index] in pieces_dict:
+                list_value = pieces_dict[arr[index]]
+                for each_value in list_value:
+                    if index < len(arr) and (each_value == arr[index]):
+                        index +=1
+                    else:
+                        return False
+            else:
+                return False
+
+        return True
 
 s= Solution()
-print(s.canFormArray(arr = [49,18,16], pieces = [[16,18,49]]))
+print(s.canFormArray(arr = [91,4,64,78], pieces = [[78],[4,64],[91]]))
