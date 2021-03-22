bad1b20295f297c0dc6ce38dd974d62c0a00b09d
Niharika Goel <niharika.coer@gmail.com>
2021-03-22 18:41:23 +0530

    20 Changes in pop(), default last value taken

--- python/20_valid_parentheses.py
+++ python/20_valid_parentheses.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: bool
         """
         stack=[]
-
         for i in range(0, len(s)):
             if len(stack)!=0 and s[i]==')' and stack[-1] == '(':
-                stack.pop(-1)
+                stack.pop()
             elif len(stack)!=0 and s[i]=='}' and stack[-1] == '{':
-                stack.pop(-1)
+                stack.pop()
             elif len(stack)!=0 and s[i]==']' and stack[-1] == '[':
-                stack.pop(-1)
+                stack.pop()
             else:
                 stack.append(s[i])
 
