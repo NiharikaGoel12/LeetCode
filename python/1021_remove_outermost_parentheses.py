13b52e8b6b6bace909a3218cde3b7f226d0cc02d
Niharika Goel <niharika.coer@gmail.com>
2021-03-23 11:49:16 +0530

    1021 Remove outer most parentheses

--- python/1021_remove_outermost_parentheses.py
+++ python/1021_remove_outermost_parentheses.py
@@ -4,8 +4,28 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
-        
+        counter=0
+        s_list=[]
+        result=""
 
+        for i in range(0, len(S)):
+            if S[i] == '(':
+                counter +=1
+                s_list.append(S[i])
+            elif S[i] == ')' and counter !=0:
+                counter -=1
+                s_list.append(S[i])
+            if counter ==0:
+                s_list.pop(0)
+                s_list.pop(-1)
+                output_list = ''.join(s_list)
+                result +=output_list
+                s_list = []
+
+        return result
 
 s=Solution()
-print(s.removeOuterParentheses(S= "(()())(())"))
+print(s.removeOuterParentheses(S= "(()())(())(()(()))"))
+
+
+
