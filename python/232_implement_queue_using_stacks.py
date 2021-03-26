2de3066e36149cf3bf5ca71b20b636efbb91d8ab
Niharika Goel <niharika.coer@gmail.com>
2021-03-26 12:13:31 +0530

    232 Implement Queue using Stacks

--- python/232_implement_queue_using_stacks.py
+++ python/232_implement_queue_using_stacks.py
@@ -4,7 +4,7 @@ class MyQueue(object):
         """
         Initialize your data structure here.
         """
-        
+        self.output_list=[]
 
     def push(self, x):
         """
@@ -12,24 +12,31 @@ class MyQueue(object):
         :type x: int
         :rtype: None
         """
+        self.output_list.append(x)
 
     def pop(self):
         """
         Removes the element from in front of queue and returns that element.
         :rtype: int
         """
+        return self.output_list.pop(0)
 
     def peek(self):
         """
         Get the front element.
         :rtype: int
         """
+        return self.output_list[0]
 
     def empty(self):
         """
         Returns whether the queue is empty.
         :rtype: bool
         """
+        if len(self.output_list)==0:
+            return True
+        else:
+            return False
 
 # Your MyQueue object will be instantiated and called as such:
 obj = MyQueue()
