9dcd89ab5eb3a12110389e05811c1b86ff1b8342
Niharika Goel <niharika.coer@gmail.com>
2021-03-31 18:28:49 +0530

    1800 Maximum Ascending Subarray Sum

--- python/1800_max_ascending_subarray_sum.py
+++ python/1800_max_ascending_subarray_sum.py
@@ -4,11 +4,20 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
 
+        running_sum = nums[0]
+        result=0
 
-
-
+        if len(nums) ==1:
+                return nums[0]
+        else:
+            for i in range(1, len(nums)):
+                if nums[i] > nums[i-1]:
+                    running_sum += nums[i]
+                else:
+                    running_sum = nums[i]
+                result = max(result, running_sum)
+            return result
 
 s=Solution()
-print(s.maxAscendingSum(nums = [10,20,30,5,10,50]))
+print(s.maxAscendingSum(nums = [5,5,6,6,6,9,1,2]))
