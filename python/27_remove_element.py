1ec38bd0d0fb80820bb4b3499318056ffa16c774
Niharika Goel <niharika.coer@gmail.com>
2021-03-31 22:47:45 +0530

    WIP 27 Remove element ques

---
+++ python/27_remove_element.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        pointer=0
+        nonZero_counter=len(nums)
+
+        while pointer<len(nums):
+            if nums[pointer] == val:
+                nums[pointer]=0
+                nonZero_counter -=1
+            pointer += 1
+        return nums
+
+s=Solution()
+print(s.removeElement(nums = [3,2,2,3], val = 3))
