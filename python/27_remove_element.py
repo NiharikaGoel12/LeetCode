0a0e729d863d5c3f35f4be35354be34d4e4ceb4d
Niharika Goel <niharika.coer@gmail.com>
2021-04-05 11:19:00 +0530

    27 Remove Element 2 pointers

--- python/27_remove_element.py
+++ python/27_remove_element.py
@@ -13,7 +13,19 @@ class Solution(object):
                 nums[pointer]=0
                 nonZero_counter -=1
             pointer += 1
-        return nums
+
+        insertPos=0
+        for i in nums:
+            if i !=0:
+                nums[insertPos] = i
+                insertPos +=1
+
+        while insertPos <len(nums):
+            nums[insertPos]=0
+            insertPos+=1
+
+        return nonZero_counter
 
 s=Solution()
 print(s.removeElement(nums = [3,2,2,3], val = 3))
+
