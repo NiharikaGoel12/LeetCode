0e96a08e0801436a83556a55ff4b6a6f972c302e
Niharika Goel <niharika.coer@gmail.com>
2021-04-05 15:51:44 +0530

    125 Valid Palindrome

---
+++ python/125_valid_palindrome.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        revised_s=[]
+        for i in s.lower():
+            if i.isalnum():
+                revised_s.append(i)
+
+        # return revised_s[int(len(revised_s)/2)]
+        if len(revised_s)%2 !=0:
+            pal_1= int(len(revised_s)/2)-1
+            pal_2 = int(len(revised_s)/2)+1
+        else:
+            pal_1 = int(len(revised_s) / 2) - 1
+            pal_2 = int(len(revised_s) / 2)
+
+        while pal_1>=0 and pal_2<=len(revised_s):
+            if revised_s[pal_1]!= revised_s[pal_2]:
+                return False
+            else:
+                pal_1 -=1
+                pal_2 +=1
+
+        return True
+
+s=Solution()
+print(s.isPalindrome(s = "0P"))
