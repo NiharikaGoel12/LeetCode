02285759f31da5010d7ef98162a5d230ebebda77
Niharika Goel <niharika.coer@gmail.com>
2021-04-08 23:18:10 +0530

    100 Same Tree

---
+++ python/100_same_tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from python.leetcode_tree_helper import deserialize
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if p is None or q is None:
+            if p is None and q is None:
+                return True
+            else:
+                return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
+s=Solution()
+p = deserialize('[1,2]')
+q = deserialize('[1,null,2]')
+print(s.isSameTree(p,q))
