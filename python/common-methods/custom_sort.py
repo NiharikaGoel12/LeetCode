d53f2ad58543a50219bf59f60bbef21965ac0a81
Niharika Goel <niharika.coer@gmail.com>
2021-04-16 11:40:39 +0530

    update custom sort example

--- python/common-methods/custom_sort.py
+++ python/common-methods/custom_sort.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 elements = [
     ("a", 1, "aneeraj"),
     ("b", 2, "x"),
@@ -10,9 +10,11 @@ elements = [
 
 
 
-sort_compare = lambda  x : x
+# pass tuple if sorting needs to be done on basis on multiple params.
+# Next parameter will only be considered if values equal for previous parameter
+sort_compare = lambda  x : (x[1], x[0])
 
-print(sorted(elements, key= sort_compare))
+print(sorted(elements, key= sort_compare, reverse=True))
 
 '''
 if a == b : return 0 // which mains both values
@@ -20,3 +22,12 @@ if a < b :
   return -1
 else 1 // a > b
 '''
+
+# s = "leetcode"
+# freq_map = Counter(s)
+# s_list = list(s)
+# s_list.sort(key=lambda x:freq_map[x], reverse=True)
+# print("".join(s_list))
+# sorted_list = sorted(s, key= lambda x: -1*freq_map[x])
+# result = "".join(sorted_list)
+# print(result)
