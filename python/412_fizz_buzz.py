daf96abf78ad03f2b01cd0e8b5d41a1926e3df86
Niharika Goel <niharika.coer@gmail.com>
2021-04-19 15:33:54 +0530

    412 Fizz Buzz

---
+++ python/412_fizz_buzz.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result=[]
+        for index in range(1,n+1):
+            if index%3==0 or index%5==0:
+                if index%3==0 and index%5==0:
+                    result.append("FizzBuzz")
+                elif index%3==0:
+                    result.append("Fizz")
+                elif index%5==0:
+                    result.append("Buzz")
+            else:
+                result.append(str(index))
+        return result
+
+s=Solution()
+print(s.fizzBuzz(15))
